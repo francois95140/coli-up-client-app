62d24a5ac3c1ee605864613e3821576ff07d5794
François <totofrancois12@gmail.com>
2025-05-06 13:53:27 +0200

    nul

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // ← Important pour Cloudflare Pages
+    images: {
+        unoptimized: true // ← Si tu utilises <Image />, requis pour export
+    },
 };
 
 export default nextConfig;
